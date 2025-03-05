b215a8b20674155a96b7c61c9473dec9c4b08793
Augusto Recalde <arecalde@fadelsa.com.ar>
2025-03-05 14:41:59 -0300

    ignore

--- utils.js
+++ utils.js
@@ -1,2 +1,3 @@
+/** Función para retornar un valor 0 o personalizado en caso que el argumento coincida con undefined, string vacio o null */
 export const nvl = (valor, defaultValue = 0) =>
   valor === undefined || valor === '' || valor === null ? defaultValue : valor;
